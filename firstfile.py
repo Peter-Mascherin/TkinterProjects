426512e71b821378293d530bf86878fec335e49e
Peter-Mascherin <pe.mascherin@gmail.com>
2021-01-11 16:20:44 -0500

    important comments

--- firstfile.py
+++ firstfile.py
@@ -1,4 +1,6 @@
 import firstfileResources.testclass as ts
-
+#the start of my tkinter projects
+#MAIN RUN FILE MUST ALWAYS BE IN THE BASE WORKSPACE FOLDER (TkinterProjects) 
+#ANYWHERE ELSE AND THE IMPORTS WONT WORK CORRECTLY FOR GOD KNOWS WHAT REASONS
 print("hey buddy time to start tkinter")
 ts.Testmeplz.imatest()
