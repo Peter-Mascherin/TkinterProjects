336e1ff010e5ca671420fa8cd8321314f5f3c6de
Peter-Mascherin <pe.mascherin@gmail.com>
2021-01-20 22:53:48 -0500

    new tkinter practice project placeholder file

---
+++ PracticeProjectMain.py
@@ -0,0 +1,5 @@
+#"main" must be ran from main workspace folder, any folders deeper and the imports dont work for god knows what reason
+#A practice project meant to create something fun and learning Tkinter methods, classes, and styling of the app
+
+import tkinter as tk
+print("placeholder print statement")
