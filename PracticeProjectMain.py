73419058446dc488aae9f1e250813041e61fa728
Peter-Mascherin <pe.mascherin@gmail.com>
2021-01-28 16:47:01 -0500

    switch import to ttk for modern and styling

--- PracticeProjectMain.py
+++ PracticeProjectMain.py
@@ -1,10 +1,13 @@
 #"main" must be ran from main workspace folder, any folders deeper and the imports dont work for god knows what reason
 #A practice project meant to create something fun and learning Tkinter methods, classes, and styling of the app
 
-import tkinter as tk
-from tkinter.constants import *
-from tkinter.ttk import *
+#import tkinter as tk
+#from tkinter.constants import *
+#from tkinter import messagebox
+from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
+from tkinter.ttk import *
 
 def buttonchange():
     print("bye bye")
@@ -13,19 +16,19 @@ def buttonchange():
 
 def buttonmsgbox():
     print("hi hi")
-    messagebox.showwarning(title="ALERT ALERT",message="THIS IS TKINTER OI")
+    messagebox.showerror(title="ALERT ALERT",message="YOUR DED FAGGOT LUL")
 
 def maybenewwindow():
     print("new window coming right up")
-    newwindow = tk.Tk()
-    newlabel = tk.Label(newwindow,text="Im a new window, what can i do in here").pack()
-    newbutton = tk.Button(newwindow,text="What will this button do???",command=lambda: makelabel(2)).pack()
+    newwindow = Tk()
+    newlabel = Label(newwindow,text="Im a new window, what can i do in here").pack()
+    newbutton = Button(newwindow,text="What will this button do???",command=lambda: makelabel(2)).pack()
     newwindow.title("Im a new window")
     newwindow.geometry("300x100")
     
 
 def makelabel(num):
-    labelA1 = tk.Label(root)
+    labelA1 = Label(root)
     if(num == 1):
         labelA1.config(text="This label was made in the root window")
     elif(num == 2):
@@ -35,11 +38,11 @@ def makelabel(num):
 
       
 def makeinputwindow():
-    inputwindow = tk.Tk()
-    inputlabel = tk.Label(inputwindow,text="Input text to show up on main window :)")
-    inputtext = tk.Entry(inputwindow)
+    inputwindow = Tk()
+    inputlabel = Label(inputwindow,text="Input text to show up on main window :)")
+    inputtext = Entry(inputwindow)
     inputtext.focus()
-    inputbutton = tk.Button(inputwindow,text="Submit",command=lambda: showinputtext(inputtext.get()))
+    inputbutton = Button(inputwindow,text="Submit",command=lambda: showinputtext(inputtext.get()))
     inputlabel.pack(side=TOP)
     inputtext.pack()
     inputbutton.pack(side=BOTTOM)
@@ -47,18 +50,17 @@ def makeinputwindow():
     inputwindow.title("Im the input window")
           
 def showinputtext(thetext):
-    theinputtext = tk.Label(root,text=thetext)
+    theinputtext = Label(root,text=thetext)
     theinputtext.pack()
 
-root = tk.Tk()
+root = Tk()
 root.title("Practice Project for widgets and style")
-label = tk.Label(root,text="Ohaiyo Gozaimas").pack()
-firstbutton = tk.Button(root,text="Click to close!",command=buttonchange).pack()
-secondbutton = tk.Button(root,text="Click to show message box!",command=buttonmsgbox).pack()
-thirdbutton = tk.Button(root,text="Magic man?",command=maybenewwindow).pack()
-fourthbutton = tk.Button(root,text="Click me for Labels",command=lambda: makelabel(1)).pack()
-fifthbutton = tk.Button(root,text="Click me to open an input window",command=lambda: makeinputwindow()).pack()
-
+label = Label(root,text="Ohaiyo Gozaimas").pack()
+firstbutton = Button(root,text="Click to close!",command=buttonchange).pack()
+secondbutton = Button(root,text="Click to show message box!",command=buttonmsgbox).pack()
+thirdbutton = Button(root,text="Magic man?",command=maybenewwindow).pack()
+fourthbutton = Button(root,text="Click me for Labels",command=lambda: makelabel(1)).pack()
+fifthbutton = Button(root,text="Click me to open an input window",command=lambda: makeinputwindow()).pack()
 
 root.geometry("500x300+900+300")
 root.mainloop()
