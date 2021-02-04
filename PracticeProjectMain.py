96a305351b7735810d2d37d4848f00c507c45d2c
Peter-Mascherin <pe.mascherin@gmail.com>
2021-02-04 15:52:53 -0500

    last commit and update to practiceproject

--- PracticeProjectMain.py
+++ PracticeProjectMain.py
@@ -8,7 +8,7 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from tkinter.ttk import *
-
+#so overall ttk tkinter is shit, stick with regular tkinter cant even style buttons properly with ttk
 def buttonchange():
     print("bye bye")
     root.destroy()
@@ -33,8 +33,9 @@ def makelabel(num):
         labelA1.config(text="This label was made in the root window")
     elif(num == 2):
         labelA1.config(text="This label was actually made in the new window")
-    
-    labelA1.pack()
+    num = num + 1
+    labelA1.grid(row=num,column=0)
+   
 
       
 def makeinputwindow():
@@ -54,15 +55,17 @@ def showinputtext(thetext):
     theinputtext.pack()
 
 root = Tk()
+root.configure(bg="#4E4A4A")
 style = ttk.Style()
 style.configure("TButton",background="red",foreground="black")
 padding = 10
+colouring = "#6F5E5E"
 root.title("Practice Project for widgets and style")
 label = Label(root,text="Ohaiyo Gozaimas").grid(row=0,column=0,padx=padding,pady=padding)
 firstbutton = Button(root,text="Click to close!",command=buttonchange).grid(row=0,column=1,padx=padding,pady=padding)
 secondbutton = Button(root,text="Click to show message box!",command=buttonmsgbox).grid(row=0,column=2,padx=padding,pady=padding)
 thirdbutton = Button(root,text="Magic man?",command=maybenewwindow).grid(row=1,column=0,padx=padding,pady=padding)
-fourthbutton = ttk.Button(root,text="Click me for Labels",command=lambda: makelabel(1),style="TButton").grid(row=1,column=1,padx=padding,pady=padding)
+fourthbutton = ttk.Button(root,text="Click me for Labels",command=lambda: makelabel(1)).grid(row=1,column=1,padx=padding,pady=padding)
 fifthbutton = Button(root,text="Click me to open an input window",command=lambda: makeinputwindow()).grid(row=1,column=2,padx=padding,pady=padding)
 
 root.geometry("500x300+900+300")
