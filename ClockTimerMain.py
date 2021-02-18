8605151080020e9663c47e9ebae9cb8a0e0917cb
Peter-Mascherin <pe.mascherin@gmail.com>
2021-02-18 13:21:09 -0500

    placeholder and structure for ClockTimerMain

---
+++ ClockTimerMain.py
@@ -0,0 +1,19 @@
+#Clock and Timer app created by Peter Mascherin
+#A general purpose clock and timer app i plan to provide continous development on
+#Shows the time, can set a timer, a stopwatch, and future functions to be added
+#Current code is placeholder code
+import tkinter as tk
+from tkinter.constants import *
+
+root = tk.Tk()
+
+
+
+
+root.configure(bg="#363535")
+root.title("Clock and Timer App")
+root.geometry("500x200")
+root.iconbitmap(r'ClockTimerResources\alarmclock.ico')
+root.mainloop()
+
+
