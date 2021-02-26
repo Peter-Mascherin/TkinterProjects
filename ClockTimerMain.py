607d71d89eb5194f2152940dbd0ccd9447fad9b0
Peter-Mascherin <pe.mascherin@gmail.com>
2021-02-26 11:13:48 -0500

    proper date and time functionality

--- ClockTimerMain.py
+++ ClockTimerMain.py
@@ -4,13 +4,40 @@
 #Current code is placeholder code
 import tkinter as tk
 from tkinter.constants import *
+from time import *
+from tkinter.font import *
+from datetime import date, datetime
 
 root = tk.Tk()
+textfont = Font(family="Helvetica",size=36)
+rootbackground = "#363535"
 
+def clockupdate():
+    
+    timestring = datetime.today().strftime("%I:%M:%S %p %Z")
+    clocklabel.config(text=timestring[1:])
+    clocklabel.after(100,clockupdate)
 
+def stopwatchupdate():
+    stopwatchstring = datetime.today().strftime('%I:%M:%S:%f')[:-3]
+    stopwatchlabel.configure(text=stopwatchstring)
+    stopwatchlabel.after(10,stopwatchupdate)
+    
 
+clocklabel = tk.Label(root,bg=rootbackground,fg="#FFFFFF",text="testest")
+clocklabel.configure(font=textfont,padx=10,pady=10)
+clocklabel.grid(row=0,column=0,sticky=(N,W),columnspan=6)
+clockupdate()
 
-root.configure(bg="#363535")
+stopwatchlabel = tk.Label(root,bg=rootbackground,fg="#FFFFFF",text="placehgolder text")
+stopwatchlabel.configure(font=textfont,pady=10,padx=10)
+stopwatchlabel.grid(row=5,column=5,sticky=(S,E),columnspan=6)
+stopwatchupdate()
+
+root.rowconfigure((0,1,2,3,4,5),weight=1)
+root.columnconfigure((0,1,2,3,4,5),weight=1)
+
+root.configure(bg=rootbackground)
 root.title("Clock and Timer App")
 root.geometry("500x200")
 root.iconbitmap(r'ClockTimerResources\alarmclock.ico')
