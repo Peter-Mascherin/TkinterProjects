d59f448d46bfc791cb77f64c9a4d6385a2db31d9
Peter-Mascherin <pe.mascherin@gmail.com>
2021-03-01 19:18:45 -0500

    radiourl json file, button images, multi url func

--- RadioMain.py
+++ RadioMain.py
@@ -2,8 +2,12 @@
 #Built by peter mascherin
 
 import tkinter as tk
+from tkinter.font import *
 from tkinter.constants import *
 import vlc #pip install python-vlc (you also need the 64-bit version of VLC installed which can be downloaded here https://get.videolan.org/vlc/3.0.11/win64/vlc-3.0.11-win64.exe)
+import time
+import json
+import RadioResources.radiourls as rad
 
 #METHODS
 def stopmedia():
@@ -14,21 +18,44 @@ def pausemedia():
 
 def playmedia():
     media.play()
-
+    
+def switchstation():
+    print('yp')
 
 #WIDGET DECLARATION
 root = tk.Tk()
-playbutton = tk.Button(root,text="Play",command=lambda:playmedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=2)
-pausebutton = tk.Button(root,text="Pause/Resume",command=lambda:pausemedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=2)
-stopbutton = tk.Button(root,text="Stop",command=lambda:stopmedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=2)
+fowardimage = tk.PhotoImage(file="RadioResources/foward.png")
+backimage = tk.PhotoImage(file="RadioResources/back.png")
+stopimage = tk.PhotoImage(file="RadioResources/stop.png")
+pauseimage = tk.PhotoImage(file="RadioResources/pause.png")
+playimage = tk.PhotoImage(file="RadioResources/play.png")
+custfont = Font(family="Helivetica",size=24)
+playbutton = tk.Button(root,image=playimage,command=lambda:playmedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=40,bd=0)
+pausebutton = tk.Button(root,image=pauseimage,command=lambda:pausemedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=40,bd=0)
+stopbutton = tk.Button(root,image=stopimage,command=lambda:stopmedia(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=40,bd=0)
+nextbutton = tk.Button(root,image=fowardimage,command=lambda:switchstation(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=40,bd=0)
+previousbutton = tk.Button(root,image=backimage,command=lambda:switchstation(),bg="#3D3939",fg="#FFFFFF",activeforeground="#FFFFFF",activebackground="#3D3939",height=40,bd=0)
+testtext = tk.Label(root,text="font change me",font=custfont,fg="#FFFFFF",bg="#3D3939")
+
 
 #WIDGET PLACEMENT
-playbutton.grid(row=2,column=0,sticky=(S,W,E),pady=6,padx=2)
-pausebutton.grid(row=2,column=1,sticky=(S,E,W),pady=6,padx=2)
-stopbutton.grid(row=2,column=2,sticky=(S,E,W),pady=6,padx=2)
+playbutton.grid(row=2,column=1,sticky=(S,W,E),pady=6,padx=2)
+pausebutton.grid(row=2,column=2,sticky=(S,E,W),pady=6,padx=2)
+stopbutton.grid(row=2,column=0,sticky=(S,E,W),pady=6,padx=2)
+nextbutton.grid(row=1,column=2,sticky=(E,W),pady=2,padx=6,rowspan=2)
+previousbutton.grid(row=1,column=0,sticky=(E,W),pady=6,padx=2,rowspan=2)
+testtext.grid(row=0,column=0,sticky=(N,W),padx=6,pady=2,columnspan=3)
+
 
 #VLC MEDIA CONFIGURATION
-media = vlc.MediaPlayer("https://uk3.internet-radio.com/proxy/majesticjukebox?mp=/live")
+
+datad = json.loads(rad.urls)
+urlstore = []
+for url in datad['stations']:
+    urlstore.append(url['radiourl'])
+media = vlc.MediaPlayer(urlstore[4])
+media.audio_set_volume(80)
+
 
 #ROOT CONFIGURATION
 root.rowconfigure((0,1,2),weight=1)
---
+++ RadioResources/radiourls.py
@@ -0,0 +1,28 @@
+import json
+
+urls = '''{
+    "stations":[
+        {
+            "radiourl": "https://rogers-hls.leanstream.co/rogers/tor981.stream/icy?environment=tunein&args=tunein_01",
+            "radioname":    "98.1 CHFI"
+        },
+        {
+            "radiourl": "https://ice23.securenetsystems.net/CIDC",
+            "radioname":    "Z103.5"
+        },
+        {
+            "radiourl": "http://str.topradio.be:80/topradio.aac",
+            "radioname":    "!Superdance"
+        },
+        {
+            "radiourl": "https://uk3.internet-radio.com/proxy/majesticjukebox?mp=/live",
+            "radioname":    "Majestic Jukebox"
+        },
+        {
+            "radiourl": "http://pulseedm.cdnstream1.com:8124/1373_128",
+            "radioname":    "Pulse EDM"
+        }
+
+    ]
+}'''
+
